fa67d759905897d649ada942ba5fadf3881c41c3
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2023-06-18 22:33:11 -0500

    Rectangulo

---
+++ Colaboradores/Rafael-Fonseca/Rectangulo/Rectangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Crear un Script que calcule el area de un rectangulo
+
+double a = 0;
+double b = 0;
+double resultado = 0;
+
+
+Console.WriteLine("******** Area de un rectangulo ********");
+Console.WriteLine("Por favor ingrese un valor:");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Por favor ingrese un valor:");
+b = Convert.ToDouble(Console.ReadLine());
+
+resultado = a * b;
+
+Console.WriteLine("El área del rectangulo es:" + resultado);
