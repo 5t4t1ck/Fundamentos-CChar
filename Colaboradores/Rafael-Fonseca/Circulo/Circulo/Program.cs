b7276320a4df44fc9cca87f78366a7c8505fc673
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2023-06-18 22:34:21 -0500

    Circulo

---
+++ Colaboradores/Rafael-Fonseca/Circulo/Circulo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("******** Area de un Circulo ********");
+
+Console.WriteLine("Ingrese el radio del circulo: ");
+double radio = double.Parse(Console.ReadLine());
+
+double area = Math.PI * Math.Pow(radio, 2);
+
+Console.WriteLine($"El area del circulo es: {area}");
+
